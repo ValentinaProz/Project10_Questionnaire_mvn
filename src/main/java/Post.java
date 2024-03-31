c8f109a7b7aed091c5586c204e5a014bc53cc6ea
Valentina.Pro <verano86@mail.ru>
2024-03-31 17:09:57 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для номера телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // поле для подписки
+    public FormDate birthday;
+
+}
